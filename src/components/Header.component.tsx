245a6571a5a61e3766aef27ba069e773135615ad
Adao-Angelo <adaobegginer@gmail.com>
2024-07-23 12:17:13 +0100

    finish header menu

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -5,23 +5,26 @@ import "../assets/styles/componentsStyles/Header.css";
 
 export default function Header() {
   return (
-    <header>
-      <menu>
+    <header className="header-component">
+      <menu className="f-medium">
         <section className="logo">
           <img src={Logo} alt="Logo" />
         </section>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="color-black">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About us</Link>
+            <Link to="/about" className="color-gray200">
+              About us
+            </Link>
           </li>
         </ul>
       </menu>
-      <section className="signInOrSignUp">
+      <section className="signInOrSignUp f-medium">
         <Link to="/sign-in">Sign In</Link>
-
         <Link to="/sign-up">Sign Up</Link>
       </section>
     </header>
