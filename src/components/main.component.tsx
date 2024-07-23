a799a1f0f38500a61c9992b69b001009c0d13c3e
Adao-Angelo <adaobegginer@gmail.com>
2024-07-23 16:11:18 +0100

    create btn filter show

--- src/components/main.component.tsx
+++ src/components/main.component.tsx
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom";
 import "../assets/styles/componentsStyles/Main.css";
 import Article from "./article.component";
 import Pagination from "./pagination.component";
+import { HiOutlineFilter } from "react-icons/hi";
 export default function Main() {
   return (
     <>
       <main className="main-container">
-        <p className="f-Mc size-40 main-tittle">Articles</p>
+        <div className="top-main">
+          <p className="f-Mc size-40 main-tittle">
+            Articles{" "}
+            <span className="f-medium size-16 color-gray200">
+              (100 results)
+            </span>
+          </p>
+
+          <Link to="/" className="f-medium size-14 color-gray200 filter-btn">
+            <HiOutlineFilter />
+            show filter
+          </Link>
+        </div>
         <section className="container-articles">
           <Article></Article>
           <Article></Article>
