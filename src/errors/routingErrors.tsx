8af9d5922fdd424c8116ea6c78099414057ca286
Adao-Angelo <adaobegginer@gmail.com>
2024-07-24 03:58:58 +0100

    create details articles and about us page

--- src/errors/routingErrors.tsx
+++ src/errors/routingErrors.tsx
@@ -11,12 +11,12 @@ export default function NotFound() {
   return (
     <div className="not-found-container">
       <img src={Logo} alt="New Aggregator Logo" />
-      <h3 className="error-message">News Aggregator | Page Not Found</h3>
+      <h3 className="error-message">News Aggregator | Page Not Found</h3>;
       <p className="error-description">
         Oops! The page you are looking for does not exist. It might have been
         moved or deleted.
       </p>
-      <Link to="/" className="home-link f-medium">
+      <Link to="/" className="home-link f-medium ⚛️ ">
         Go to Home
       </Link>
     </div>
--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,10 +1,16 @@
 import Header from "../components/Header.component";
 import Footer from "../components/footer.component";
+import ArticleDetails from "../components/articleDetails";
+import { useEffect } from "react";
 
 export default function Details() {
+  useEffect(() => {
+    document.title = "News Aggregator | Details of  ";
+  }, []);
   return (
     <>
       <Header></Header>
+      <ArticleDetails></ArticleDetails>
       <Footer></Footer>
     </>
   );
