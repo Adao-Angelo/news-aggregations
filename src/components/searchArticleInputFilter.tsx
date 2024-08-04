99030a23de65a3bc6f8295864e5e9a46f20f6d6b
Adao-Angelo <adaobegginer@gmail.com>
2024-08-04 10:41:58 +0100

    create newarticle funcionality for create article dinimic

--- src/components/searchArticleInputFilter.tsx
+++ src/components/searchArticleInputFilter.tsx
@@ -3,24 +3,17 @@ import { useFilter } from "../utils/filterContext";
 export default function SearchInputComponent() {
   const { filterUrl, setFilterUrl } = useFilter();
 
-  let inputValue = filterUrl;
-
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilterUrl(event.target.value);
-  };
-
-  const changeInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    inputValue = event.target.value;
   };
 
   return (
     <>
       <input
-        value={inputValue}
         onChange={(e: any) => {
           e.preventDefault();
+          handleChange(e);
         }}
-        onKeyDown={handleChange(e)}
         className="size-12 f-regular"
         type="search"
         name="search-article-input"
