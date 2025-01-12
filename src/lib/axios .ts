ece8b21bd15705e23e92617e743ae20c72e2f2f4
Adao-Angelo <adaobegginer@gmail.com>
2025-01-12 04:11:14 +0100

    env

--- src/lib/axios .ts
+++ src/lib/axios .ts
@@ -1,16 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "api_live_tFY2u5eefYBWxENhiz5zEhft0ZjIkMCSUP4vIIsjWKdaEYt",
-  // baseURL:
-  //   import.meta.env.VITE_API_BASE_URL ||
-  //   "api_live_tFY2u5eefYBWxENhiz5zEhft0ZjIkMCSUP4vIIsjWKdaEYt",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use(
   (config) => {
-    // const apiKey = import.meta.env.VITE_API_TUBE_KEY || "/api/v1/news";
-    const apiKey = "/api/v1/news";
+    const apiKey = import.meta.env.VITE_API_TUBE_KEY;
 
     config.params = {
       ...config.params,
