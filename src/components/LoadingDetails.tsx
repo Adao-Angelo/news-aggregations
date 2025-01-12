579af7d36ce6cd38612304a00d2d2ddb35168114
Adao-Angelo <adaobegginer@gmail.com>
2025-01-12 20:51:18 +0100

    create detaisl list articles

---
+++ src/components/LoadingDetails.tsx
@@ -0,0 +1,22 @@
+export default function Loading() {
+  return (
+    <div className="animate-pulse space-y-[6rem]">
+      <div className="h-[4rem] bg-gray-300 rounded w-3/4"></div>
+      <div className="flex justify-between">
+        <div className="h-[2rem] bg-gray-300 rounded w-[50%]"></div>
+        <div className="h-[2rem] bg-gray-300 rounded-full w-[30%]"></div>
+      </div>
+
+      <div className="h-[50rem] bg-gray-300 rounded w-full"></div>
+
+      <div className="h-[3rem] bg-gray-300 rounded w-[100%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[70%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[80%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[90%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[30%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[80%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[60%]"></div>
+      <div className="h-[3rem] bg-gray-300 rounded w-[20%]"></div>
+    </div>
+  );
+}
