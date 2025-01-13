9c820e9216da8836c3f355137cce8cbe5484e354
Adao-Angelo <adaobegginer@gmail.com>
2025-01-13 16:01:29 +0100

    cretae page

--- src/types/index.ts
+++ src/types/index.ts
@@ -83,6 +83,53 @@ export type ArticleType = {
   characters_count: number;
 };
 
+export type GitHubUserType = {
+  id: string;
+  nodeId: string;
+  displayName: string;
+  username: string;
+  profileUrl: string;
+  photos: { value: string }[];
+  provider: string;
+  _raw: string;
+  _json: {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    user_view_type: string;
+    site_admin: boolean;
+    name: string;
+    company: string | null;
+    blog: string;
+    location: string;
+    email: string | null;
+    hireable: boolean | null;
+    bio: string;
+    twitter_username: string | null;
+    notification_email: string | null;
+    public_repos: number;
+    public_gists: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    updated_at: string;
+  };
+};
+
 export type FiltersType = {
   category?: string;
   source?: string;
